57438ce670543de4beb113bcfcb274eceb0a1f21
lucasbailey <lcbailey@uvm.edu>
2025-01-17 13:15:58 -0500

    added p.setGravity(0,0,-9.8)
     to simulate.py

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import time
 physicsClient = p.connect(p.GUI)
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+p.setGravity(0,0,-9.8)
+
 p.loadSDF("box.sdf")
 
 for i in range(1000):
@@ -11,6 +13,5 @@ for i in range(1000):
     time.sleep(1/60)
     print(i)
 	
-
 
 p.disconnect()
