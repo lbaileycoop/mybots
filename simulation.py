99701bebef8950bef3dda829d235825be37dbc8d
lucasbailey <lcbailey@uvm.edu>
2025-01-28 17:48:47 -0500

    added all new files

---
+++ simulation.py
@@ -0,0 +1,10 @@
+from world import WORLD
+from robot import ROBOT
+
+
+class SIMULATION:
+
+    def __init__(self):
+        # creates a new SIMULATION attribute, and that attribute will hold an instance of the WORLD class
+        self.world = WORLD()
+        self.world = ROBOT()
