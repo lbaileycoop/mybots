fd4d53705ac525222ddb52beb173e84216b63ce4
lucasbailey <lcbailey@uvm.edu>
2025-04-10 20:50:55 -0400

    more small fixes

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 # setting all variables needed with appropriate names
-vectorSize = 3000
+vectorSize = 4000
 Amplitude = np.pi / 4
 Frequency = 2
 PhaseOffset = np.pi
 simulationSpeed = 1 / 240
-numberOfGenerations = 50
+numberOfGenerations = 100
 populationSize = 10
 numSensorNeurons = 4
 numMotorNeurons = 8
--- robot.py
+++ robot.py
@@ -44,14 +44,6 @@ class ROBOT:
             # print(jointName)
             self.motors[jointName] = MOTOR(jointName)
     def Act(self, t, robotId):
-        # for neuronName in self.nn.Get_Neuron_Names():
-        #     if self.nn.Is_Motor_Neuron(neuronName):
-        #         jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
-        #         desiredAngle = self.nn.Get_Value_Of(neuronName)
-        #         desiredAngle = desiredAngle * c.motorJointRange
-        #         self.motors[jointName].Set_Value(desiredAngle, robotId)
-        #         # print(neuronName, jointName, desiredAngle)
-
         for neuronName in self.nn.Get_Neuron_Names():
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
